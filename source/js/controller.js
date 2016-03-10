a805cddba0d76b232ef2b4cdb1d5d09f7bf10344
TJ Draper <tj@buzzingpixel.com>
2016-03-10 13:31:00 -0600

    Install Fabricator for Addons base

---
+++ source/js/controller.js
@@ -0,0 +1,5 @@
+(function(F) {
+	'use strict';
+
+	F.controller = {};
+})(window.CUSTOM_MENU);
---
+++ source/js/fab.js
@@ -0,0 +1,7 @@
+// Make sure FAB is defined
+window.CUSTOM_MENU = window.CUSTOM_MENU || {};
+
+// Make sure required FAB arrays and objects have been initialized
+window.CUSTOM_MENU.exec = window.CUSTOM_MENU.exec || [];
+window.CUSTOM_MENU.vars = window.CUSTOM_MENU.vars || {};
+window.CUSTOM_MENU.lang = window.CUSTOM_MENU.lang || {};
